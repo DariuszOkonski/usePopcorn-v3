bb4f4c9d71ea496fde7cac0b3fde9dc9501fe745
Dariusz Okoński <darek200180@gmail.com>
2025-04-03 09:41:11 +0200

    add basic StarRating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import './index.css';
+import StarRating from './StarRating/StarRating.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
+  </StrictMode>
+);
