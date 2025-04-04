823e04be66dbe6709c0f3d052b0c47e353b4c2d5
Dariusz Okoński <darek200180@gmail.com>
2025-04-04 09:04:21 +0200

    add default props to StartRating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import StarRating from './StarRating/StarRating.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'OKay', 'Good', 'Amazing']}
+    />
+    <StarRating
+      maxRating={10}
+      size={24}
+      color='red'
+      className='test'
+      defaultRating={3}
+    />
   </StrictMode>
 );
