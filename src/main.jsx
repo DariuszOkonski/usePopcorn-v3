ae2df380f84d52a28821c503bc23908e53e34167
Dariusz Okoński <darek200180@gmail.com>
2025-04-04 09:16:50 +0200

    display App

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,7 @@
-import React, { StrictMode, useState } from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import './index.css';
-import StarRating from './StarRating/StarRating.jsx';
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating maxRating={10} color='blue' onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -20,18 +9,6 @@ if (!rootElement) {
 }
 createRoot(rootElement).render(
   <StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={['Terrible', 'Bad', 'OKay', 'Good', 'Amazing']}
-    />
-    <StarRating
-      maxRating={10}
-      size={24}
-      color='red'
-      className='test'
-      defaultRating={3}
-    />
-
-    <Test />
+    <App />
   </StrictMode>
 );
