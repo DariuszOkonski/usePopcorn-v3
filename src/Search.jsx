e5b95514961178139449e4ead39ccbe049935a91
Dariusz Okoński <darek200180@gmail.com>
2025-04-23 09:37:56 +0200

    add focus on search element

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    if (inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, []);
+
   return (
     <input
+      ref={inputElement}
       className='search'
       type='text'
       placeholder='Search movies...'
